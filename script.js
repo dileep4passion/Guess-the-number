ac79d7204fa2b2ef44d0f56cdf346f6c6df39d6f
Sai Dileep <64729076+dileep4passion@users.noreply.github.com>
2024-03-29 11:37:18 +0530

    final guess option resolved

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ function displayGuess(guess) {
   userInput.value = "";
   guessSlot.innerHTML += `${guess}, `;
   numGuess++;
-  remaining.innerHTML = `${11 - numGuess} `;
+  remaining.innerHTML = `${10 - numGuess} `;
 }
 
 function displayMessage(message) {
@@ -86,7 +86,7 @@ function newGame() {
     prevGuess = [];
     numGuess = 1;
     guessSlot.innerHTML = "";
-    remaining.innerHTML = `${11 - numGuess} `;
+    remaining.innerHTML = `${10 - numGuess} `;
     userInput.removeAttribute("disabled");
     startOver.removeChild(p);
     playGame = true;
