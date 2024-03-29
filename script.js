92b43b831fb9b93bf29b77d371f7bb669ce5852d
Sai Dileep <64729076+dileep4passion@users.noreply.github.com>
2024-03-29 14:46:27 +0530

    edited some issues

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const startOver = document.querySelector(".resultParas");
 const p = document.createElement("p");
 
 let prevGuess = [];
-let numGuess = 1;
+let numGuess = 0;
 
 let playGame = true;
 
@@ -26,11 +26,11 @@ if (playGame) {
 
 function validateGuess(guess) {
   if (isNaN(guess)) {
-    alert("PLease enter a valid number");
+    alert("Please enter a valid number");
   } else if (guess < 1) {
-    alert("PLease enter a number more than 1");
+    alert("Please enter a number more than 1");
   } else if (guess > 100) {
-    alert("PLease enter a  number less than 100");
+    alert("Please enter a  number less than 100");
   } else {
     prevGuess.push(guess);
     if (numGuess === 10) {
@@ -60,7 +60,8 @@ function displayGuess(guess) {
   userInput.value = "";
   guessSlot.innerHTML += `${guess}, `;
   numGuess++;
-  remaining.innerHTML = `${11 - numGuess} `;
+  remaining.innerHTML = `${10 - numGuess} `;
+  
 }
 
 function displayMessage(message) {
@@ -84,12 +85,12 @@ function newGame() {
     console.clear();
     console.log(`new number is ${randomNumber}`);
     prevGuess = [];
-    numGuess = 1;
+    numGuess = 0;
     guessSlot.innerHTML = "";
-    remaining.innerHTML = `${11 - numGuess} `;
+    remaining.innerHTML = `${10 - numGuess} `;
     userInput.removeAttribute("disabled");
     startOver.removeChild(p);
     playGame = true;
-    displayMessage(' ')
+    displayMessage(" ");
   });
 }
